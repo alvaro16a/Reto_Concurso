246edc54177c5923d7e88f7aaea9f8f96cc2eb59
Alvaro <alvaro16a@gmail.com>
2021-12-20 22:16:02 -0500

    Se creo la clase Pregunta

---
+++ Pregunta.py
@@ -0,0 +1,11 @@
+class pregunta():
+   
+    def __init__(self, nivel):
+        """
+        Constructor de la clase jugador recibe el nombre y el id del jugador
+        """
+        self.pregunta=str
+        self.nivel = nivel
+        self.respuesta_correcta = str
+        self.opciones = []
+        self.premio= 0
